4bf2901fa86a8f3aee41b87b66da43b3a592ae92
lic <1099959038@qq.com>
2019-01-28 16:50:57 +0800

    Initial commit

---
+++ boot-suer-service-provider/src/main/java/com/lic/gmall/BootSuerServiceProviderApplication.java
@@ -0,0 +1,17 @@
+package com.lic.gmall;
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDubbo(scanBasePackages="com.lic.gmall")
+@EnableDubbo
+public class BootSuerServiceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootSuerServiceProviderApplication.class, args);
+	}
+
+}
+
