93891d8dca2d15a04c73a11b467c3d383f9fdf7a
tom_glb <1099959038@qq.com>
2020-06-02 21:44:18 +0800

    0602tt

--- gmall-interface/src/main/java/com/lic/gmall/bean/UserAddress.java
+++ gmall-interface/src/main/java/com/lic/gmall/bean/UserAddress.java
@@ -10,7 +10,7 @@ import java.io.Serializable;
 public class UserAddress implements Serializable {
 	
 	private Integer id;
-    private String userAddress; //用户地址
+    private String userAddress; //用户地址2
     private String userId; //用户id
     private String consignee; //收货人
     private String phoneNum; //电话号码
